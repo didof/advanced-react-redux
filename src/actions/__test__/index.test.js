0a2918aae8a440f807353e6e5dae27e64b96636b
prostagma-code <didonato.fr@gmail.com>
2020-11-25 18:30:29 +0100

    action testing

--- src/actions/__test__/index.test.js
+++ src/actions/__test__/index.test.js
@@ -3,13 +3,11 @@ import { SAVE_COMMENT } from 'actions/types';
 
 describe('action: saveComment', () => {
 	const testStr = 'test';
-	const action = saveComment(SAVE_COMMENT, testStr);
-	console.log(action);
-	// test('should have the correct type', () => {
-	// 	expect(action.type).toEqual(SAVE_COMMENT);
-	// });
-	// test('should have the correct payload', () => {
-	// 	expect(action.payload).toEqual(testStr);
-	// });
-	test('si', () => {});
+	const action = saveComment(testStr);
+	test('should have the correct type', () => {
+		expect(action.type).toEqual(SAVE_COMMENT);
+	});
+	test('should have the correct payload', () => {
+		expect(action.payload).toEqual(testStr);
+	});
 });
